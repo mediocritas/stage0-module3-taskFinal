75f1fed465a82705c8b6a6d1ee2c295ecc8d8ae2
Olga Pushkina <emmanuilaklein@gmail.com>
2022-09-14 20:10:28 +0300

    All is done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sumOfDigits = 0;
+
+        for (int i = 1; i <= 4; i++) {
+            sumOfDigits += number % 10;
+            number = number / 10;
+        }
+        System.out.println(sumOfDigits);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+
+        for (int i = 1; i < 3; i++) {
+            System.out.print(number % 10);
+            number = number / 10;
+        }
+        System.out.println(number);
     }
 }
